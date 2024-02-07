db295b03191a60609b35803872b5bc8111743aac
Adam Walder <walder2@llnl.gov>
2024-02-07 10:57:19 -0800

    - update to README.md - recipe_website_list.txt added to recipe_data. - nutrition and prep extraction templates added to extract_ingredients_directions - added recipe_website_dict to recipe_kg_extraction.py - new verbose features for printing out results - extract_ingredients_directions can take a list of html files and return a list of txt_files (paths) - extract_recipe_kg can take list of txt_files (cleaned web scrapes) for triple extraction - added scrape_recipes.py for script call to extract triples from text files. - removed all previous data!

--- kg_extraction/__init__.py
+++ kg_extraction/__init__.py
@@ -4,12 +4,13 @@ __all__ = [
     "extract_ingredients_directions",
     "extract_recipe_kg",
     "kg_to_hetero",
-    "get_recipe_kg"
+    "get_recipe_kg",
+    "recipe_website_dict"
 
 ]
 
 from kg_extraction.recipe_kg_extraction import (
-    scrape_recipe_websites, extract_ingredients_directions, extract_recipe_kg
+    scrape_recipe_websites, extract_ingredients_directions, extract_recipe_kg, recipe_website_dict
 )
 
 from kg_extraction.recipe_data_load import kg_to_hetero, get_recipe_kg

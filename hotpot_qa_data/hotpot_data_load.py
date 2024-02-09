4da793a6591e6c730972a98f9b221deea963f111
Adam Walder <walder2@llnl.gov>
2024-02-08 16:02:29 -0800

    changed names of some loading/cleaning functions added write_hotpot_to_txt and load_hotpot_kgs hotpot_qa_kgs.py to extract triples

--- hotpot_qa_data/hotpot_data_load.py
+++ hotpot_qa_data/hotpot_data_load.py
@@ -1,11 +1,21 @@
 import os
+
+import pandas as pd
 from datasets import load_dataset
 import string
 import re
 import json
-from typing import Dict, Tuple, List
+from typing import Dict, List
+import numpy as np
 
-def load_hotpot_qa(path_to_data: str, ndocs: int = 2) -> Dict[str, Dict[str, List[str]]]:
+def write_hotpot_to_txt(path_to_data: str, ndocs: int = 10) -> Dict[str, Dict[str, List[str]]]:
+    """Write out hotpot qa entries to a txt file.
+
+    :param path_to_data: Path to './hotpot_qa_data/txt_files'
+    :param ndocs: Number of question/amswer pairs to write to txt.
+    :return: Dict mapping question id to the set of context names and file paths for each context txt file.
+
+    """
     dataset = load_dataset("hotpot_qa", "distractor")
     hotpot_json = '/'.join([path_to_data, 'hotpot_qa_data.json'])
     if not os.path.exists(hotpot_json):
@@ -36,3 +46,45 @@ def load_hotpot_qa(path_to_data: str, ndocs: int = 2) -> Dict[str, Dict[str, Lis
             hotpot_files = json.load(f)
 
     return hotpot_files
+
+def load_hotpot_kgs(path_to_data: str) -> pd.DataFrame:
+    """Read the hotpot_qa json kg triples from ./kg_files to DataFrame.
+
+    `doc_id` is the id for the group (question group id), `sub_idx` is the id of the subgraph extracted for
+        context entry `j` for a particular question.
+
+
+    :param path_to_data: Path to './hotpot_qa_data/txt_files'
+    :return: DataFrame with extracted subgraphs.
+    """
+    hotpot_json = '/'.join([path_to_data, 'hotpot_qa_data.json'])
+    if os.path.exists(hotpot_json):
+        with open(hotpot_json, 'r') as f:
+            hotpot_files = json.load(f)
+
+        kgs = []
+        kg_idx = []
+        qa_idx = []
+        titles = []
+        for i, hid in enumerate(hotpot_files.keys()):
+
+            for j, file in enumerate(hotpot_files[hid]['file_paths']):
+                file_name = re.sub('.txt', '.json', re.sub('txt_files', 'kg_files', file))
+
+                if os.path.exists(file_name):
+                    with open(file_name, 'r') as f:
+                        tmp = json.load(f)
+                        kg_idx.extend([j]*len(tmp))
+                        qa_idx.extend([i]*len(tmp))
+                        kgs.extend(tmp)
+                        titles.extend([hotpot_files[hid]['topics'][j]]*len(tmp))
+
+        y = pd.DataFrame(kgs)
+        y = y.assign(titles=titles)
+        y = y.assign(doc_id=qa_idx)
+        y = y.assign(sub_idx=kg_idx)
+
+        return y
+
+    else:
+        raise Exception('You need to scrape the txt_files and write to kg_files.')

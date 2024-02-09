b09855ef7fb6de45595541497b14947f24aa64bf
Adam Walder <walder2@llnl.gov>
2024-02-09 12:23:26 -0800

    - annotated notebooks - hotpot_kg_rag.ipynb added for RAG with KGs - query_answer.json added and json.dump() added to hotpot_data_load.py load_hotpot_kgs and write_hotpot_to_txt

--- hotpot_qa_data/hotpot_data_load.py
+++ hotpot_qa_data/hotpot_data_load.py
@@ -5,7 +5,7 @@ from datasets import load_dataset
 import string
 import re
 import json
-from typing import Dict, List
+from typing import Dict, List, Tuple, Optional
 import numpy as np
 
 def write_hotpot_to_txt(path_to_data: str, ndocs: int = 10) -> Dict[str, Dict[str, List[str]]]:
@@ -22,13 +22,14 @@ def write_hotpot_to_txt(path_to_data: str, ndocs: int = 10) -> Dict[str, Dict[st
 
         pattern = r"[{}]".format(string.punctuation)
         hotpot_files = dict()
+        query_answer = []
 
         for i in range(ndocs):
 
             x = dataset['train'][i]
             topics = [re.sub(pattern, '', y).lower() for y in x['context']['title']]
             hotpot_files[x['id']] = {'topics': [], 'file_paths': []}
-            # print(topics, [re.sub(' ', '_', topics[j]) for j in range(len(topics))])
+            query_answer.append({'query': x['query'], 'answer': x['answer']})
 
             for j in range(len(topics)):
                 file_name = '/'.join([path_to_data, 'txt_files', re.sub(' ', '_', topics[j]) + '.txt'])
@@ -41,13 +42,18 @@ def write_hotpot_to_txt(path_to_data: str, ndocs: int = 10) -> Dict[str, Dict[st
 
         with open(hotpot_json, 'w') as f:
             json.dump(hotpot_files, f)
+
+        with open('/'.join([path_to_data, 'query_answer.json']), 'w') as f:
+            json.dump(query_answer, f)
+
     else:
         with open(hotpot_json, 'r') as f:
             hotpot_files = json.load(f)
 
     return hotpot_files
 
-def load_hotpot_kgs(path_to_data: str) -> pd.DataFrame:
+def load_hotpot_kgs(path_to_data: str, query_answer: bool = False) -> \
+        Tuple[pd.DataFrame,  Optional[List[Dict[str, str]]]]:
     """Read the hotpot_qa json kg triples from ./kg_files to DataFrame.
 
     `doc_id` is the id for the group (question group id), `sub_idx` is the id of the subgraph extracted for
@@ -55,6 +61,7 @@ def load_hotpot_kgs(path_to_data: str) -> pd.DataFrame:
 
 
     :param path_to_data: Path to './hotpot_qa_data/txt_files'
+    :param query_answer: If True, returns list of dictionary of containing query/answer pairs for subset of data.
     :return: DataFrame with extracted subgraphs.
     """
     hotpot_json = '/'.join([path_to_data, 'hotpot_qa_data.json'])
@@ -65,7 +72,7 @@ def load_hotpot_kgs(path_to_data: str) -> pd.DataFrame:
         kgs = []
         kg_idx = []
         qa_idx = []
-        titles = []
+        file_paths = []
         for i, hid in enumerate(hotpot_files.keys()):
 
             for j, file in enumerate(hotpot_files[hid]['file_paths']):
@@ -77,14 +84,19 @@ def load_hotpot_kgs(path_to_data: str) -> pd.DataFrame:
                         kg_idx.extend([j]*len(tmp))
                         qa_idx.extend([i]*len(tmp))
                         kgs.extend(tmp)
-                        titles.extend([hotpot_files[hid]['topics'][j]]*len(tmp))
+                        file_paths.extend([hotpot_files[hid]['file_paths'][j]]*len(tmp))
 
         y = pd.DataFrame(kgs)
-        y = y.assign(titles=titles)
+        y = y.assign(file_path=file_paths)
         y = y.assign(doc_id=qa_idx)
         y = y.assign(sub_idx=kg_idx)
 
-        return y
+        if query_answer:
+            with open('/'.join([path_to_data, 'query_answer.json']), 'r') as f:
+                qa = json.load(f)
+            return y, qa
+        else:
+            return y
 
     else:
         raise Exception('You need to scrape the txt_files and write to kg_files.')

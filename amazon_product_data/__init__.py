0eb985c3370476f9727c70e93192e28b0cac3d78
Adam Walder <walder2@llnl.gov>
2024-02-23 11:46:27 -0800

    - extract_ingredients_directions changed to extract_recipe_content - added TripleExtractor class to extraction_prompts.py - recipe triples and recipe data now queried async - amazon_product.ipynb added - new folder 'scripts' containing fast runs of scrapes. - more recipes and hotpot qa examples scraped and uploaded.

--- amazon_product_data/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__ = [
-    "get_top_product_types",
-    "get_amazon_kg",
-    "amazon_kg_to_hetero"
-]
-
-from amazon_product_data.amazon_data_load import get_top_product_types, get_amazon_kg, amazon_kg_to_hetero
-
